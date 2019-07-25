00f4f28c43c7a3db53537d3f56d0b9449646c635
Shivam Tyagi <32505069+iamshivamtyagi@users.noreply.github.com>
2019-07-26 04:27:55 +0530

    result & input view updated

--- app/src/main/java/com/calculator/MainActivity.java
+++ app/src/main/java/com/calculator/MainActivity.java
@@ -1,15 +1,15 @@
 package com.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView input;
+    TextView input, showInput;
     Button doubleZero,subButton, addButton, decimal, div, multi, remainder, clear, equal, one, two, three, four, five, six, seven, eight, nine, zero;
-    boolean addition, subtraction, division, multiplication, modulus;
+    boolean addition, firstNum = false, secondNum = false, subtraction, division, multiplication, modulus;
     float val1, val2;
     TextView result;
 
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showInput = findViewById(R.id.showInput);
         doubleZero = findViewById(R.id.doubleZero);
         subButton = findViewById(R.id.subButton);
         addButton = findViewById(R.id.addButton);
@@ -42,84 +43,169 @@ public class MainActivity extends AppCompatActivity {
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "1");
+                showInput.setText(showInput.getText() + "1");
             }
         });
 
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "2");
+                showInput.setText(showInput.getText() + "2");
+                secondNum = false;
             }
         });
 
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "3");
+                showInput.setText(showInput.getText() + "3");
             }
         });
 
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "4");
+                showInput.setText(showInput.getText() + "4");
             }
         });
 
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "5");
+                showInput.setText(showInput.getText() + "5");
             }
         });
 
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "6");
+                showInput.setText(showInput.getText() + "6");
             }
         });
 
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "7");
+                showInput.setText(showInput.getText() + "7");
             }
         });
 
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "8");
+                showInput.setText(showInput.getText() + "8");
             }
         });
 
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "9");
+                showInput.setText(showInput.getText() + "9");
             }
         });
 
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "0");
+                showInput.setText(showInput.getText() + "0");
             }
         });
 
         doubleZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + "00");
+                showInput.setText(showInput.getText() + "00");
             }
         });
 
         decimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (firstNum && secondNum) {
+                    showInput.setText(null);
+                    result.setText(null);
+                    input.setText(null);
+                }
+                secondNum = false;
                 input.setText(input.getText() + ".");
+                showInput.setText(showInput.getText() + ".");
             }
         });
 
@@ -130,6 +216,8 @@ public class MainActivity extends AppCompatActivity {
                     val1 = Float.parseFloat(input.getText() + "");
                     addition = true;
                     input.setText(null);
+                    firstNum = true;
+                    showInput.setText(showInput.getText() + "+");
                 }
             }
         });
@@ -141,6 +229,8 @@ public class MainActivity extends AppCompatActivity {
                     val1 = Float.parseFloat(input.getText() + "");
                     subtraction = true;
                     input.setText(null);
+                    firstNum = true;
+                    showInput.setText(showInput.getText() + "-");
                 }
             }
         });
@@ -151,6 +241,8 @@ public class MainActivity extends AppCompatActivity {
                     val1 = Float.parseFloat(input.getText() + "");
                     multiplication = true;
                     input.setText(null);
+                    showInput.setText(showInput.getText() + "X");
+                    firstNum = true;
                 }
             }
         });
@@ -161,6 +253,8 @@ public class MainActivity extends AppCompatActivity {
                     val1 = Float.parseFloat(input.getText() + "");
                     division = true;
                     input.setText(null);
+                    showInput.setText(showInput.getText() + "/");
+                    firstNum = true;
                 }
             }
         });
@@ -170,8 +264,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 input.setText(null);
                 result.setText(null);
-                result.setVisibility(View.INVISIBLE);
-                input.setVisibility(View.VISIBLE);
+                showInput.setText(null);
+                firstNum = false;
+                secondNum = false;
             }
         });
         equal.setOnClickListener(new View.OnClickListener() {
@@ -181,25 +276,29 @@ public class MainActivity extends AppCompatActivity {
                     val2 = Float.parseFloat(input.getText() + "");
                 }
                 if (addition) {
-                    input.setVisibility(View.INVISIBLE);
                     result.setText(val1 + val2 + "");
-                    result.setVisibility(View.VISIBLE);
                     addition = false;
+                    secondNum = true;
                 } else if (subtraction) {
                     result.setText(val1 - val2 + "");
                     subtraction = false;
+                    secondNum = true;
                 } else if (multiplication) {
                     result.setText(val1 * val2 + "");
                     multiplication = false;
+                    secondNum = true;
                 } else if (division) {
                     result.setText(val1 / val2 + "");
                     division = false;
+                    secondNum = true;
                 } else if (modulus) {
                     result.setText(val1 % val2 + "");
                     modulus = false;
+                    secondNum = true;
                 }
                 else {
                     result.setText(null);
+                    secondNum = true;
                 }
             }
         });
